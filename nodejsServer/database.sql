1c99ac0bed3c7d030b4e9e325903edd560b07910
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-12-12 19:28:57 +0700

    update

--- nodejsServer/database.sql
+++ nodejsServer/database.sql
@@ -10,9 +10,34 @@ DESCRIBE users;
 --register user(
 INSERT INTO users(email, password)
 VALUES("hoang@gmail.com", "123456");
+
+DROP PROCEDURE registerUser();
+delimiter //
+CREATE PROCEDURE registerUser(email VARCHAR(250),password VARCHAR(150)) 
+BEGIN
+    INSERT INTO users(email, password) VALUES(email, MD5(password));    
+END; //
+delimiter;
+
+
+
+
+
 --trigger after insert
 DROP TRIGGER triggerCreateTokenKey;
+delimiter //
 CREATE TRIGGER triggerCreateTokenKey 
 BEFORE INSERT ON users
 FOR EACH ROW
-SET NEW.tokenKey = "aaaxxx";
+BEGIN
+    SET NEW.tokenKey = MD5(RAND());
+    SET NEW.expiredDate = DATE_ADD(CURRENT_DATE(), INTERVAL 30 DAY);    
+END; //   
+delimiter ;
+DROP PROCEDURE loginUser;
+CREATE PROCEDURE loginUser(email VARCHAR(250),password VARCHAR(150)) AS
+BEGIN
+    DECLARE numberOfUsers AS INT;
+    SELECT COUNT(*) AS numberOfUsers FROM users WHERE users.email = email AND user;
+    IF (numberOfUsers > 0)
+END;
